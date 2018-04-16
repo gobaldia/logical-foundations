8a23b6c7878f8c4ae07779988c65bf448503579e
gobaldia <gobaldia@gmail.com>
2018-04-15 21:35:49 -0300

    Avanzo hasta map_rev. Quedan pendientes rev_involutive y map_app

--- Poly.v
+++ Poly.v
@@ -744,13 +744,13 @@ Example test_filter_even_gt7_2 :
 Definition partition {X : Type}
                      (test : X -> bool)
                      (l : list X)
-                   : list X * list X
-  (* REPLACE THIS LINE WITH ":= _your_definition_ ." *). Admitted.
+                   : list X * list X :=
+  (filter test l, filter (fun x => negb (test x)) l).
 
 Example test_partition1: partition oddb [1;2;3;4;5] = ([1;3;5], [2;4]).
-(* FILL IN HERE *) Admitted.
+  reflexivity. Qed.
 Example test_partition2: partition (fun x => false) [5;9;0] = ([], [5;9;0]).
-(* FILL IN HERE *) Admitted.
+  reflexivity. Qed.
 (** [] *)
 
 (* ================================================================= *)
@@ -796,10 +796,26 @@ Proof. reflexivity.  Qed.
 (** Show that [map] and [rev] commute.  You may need to define an
     auxiliary lemma. *)
 
+Lemma map_app : forall (X Y : Type) (f : X -> Y) (l : list X) (x : X),
+  map f (rev l ++ [x]) = map f (rev l) ++ [f x].
+Proof.
+  intros X Y f l x.
+  induction l.
+  reflexivity.
+  simpl.
+Admitted.
+
 Theorem map_rev : forall (X Y : Type) (f : X -> Y) (l : list X),
   map f (rev l) = rev (map f l).
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros X Y f l.
+  induction l.
+  reflexivity.
+  simpl.
+  rewrite <- IHl.
+  rewrite <- map_app.
+  reflexivity.
+Qed.
 (** [] *)
 
 (** **** Exercise: 2 stars, recommended (flat_map)  *)
